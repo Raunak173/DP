1a3bb9ff3fd8044df8877d48e6c38110deb1a4fb
Raunak173 <raunakagarwal173@gmail.com>
2021-10-15 12:23:32 +0530

    fib using dp

---
+++ fib.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fib(int n){
+    int dp[1000]={0};
+    dp[0]=0;
+    dp[1]=1;
+
+    for(int i=2;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+
+    return dp[n];
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+    cout<<fib(n)<<endl;
+
+    return 0;
+}
