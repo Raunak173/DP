80b1c6921293b10b31826ca1c405717ca9543311
Raunak173 <raunakagarwal173@gmail.com>
2021-10-20 23:15:24 +0530

    ladders

---
+++ ladders.cpp
@@ -0,0 +1,33 @@
+//Given n steps and max jumps = k . Find no of ways to reach the top.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int ladders(int n,int k,int dp[]){
+
+    //Top down dp:
+
+    if(n==0) return 1;
+
+    if(dp[n]!=0) return dp[n];
+
+    int ways=0;
+    for(int i=1;i<=k;i++){
+        if((n-i)>=0){
+            ways+=ladders(n-i,k,dp);
+        }
+    }
+
+    return dp[n]=ways;
+}
+
+int main(){
+
+    int n,k;
+    cin>>n>>k;
+    int dp[100]={0};
+
+    cout<<ladders(n,k,dp)<<endl;
+
+    return 0;
+}
